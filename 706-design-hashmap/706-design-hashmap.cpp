f63f5348e726736ef190feac932ff87f7ebbc88c
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-04-23 00:14:33 +0530

    Time: 868 ms (5.02%), Space: 338 MB (5.02%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,20 +1,30 @@
 class MyHashMap {
 public:
-
-    int a[1000001];      
-    MyHashMap() {
-        for(int i=0;i<1000001;i++)a[i]=-1;
+    int a[10000001];
+    MyHashMap() 
+    {
+        for(int i = 0 ; i<10000001; i++)
+        {
+            a[i] = -1;   //means empty
+        }
     }
-    void put(int key, int value) {
-        a[key]=value;
+    
+    void put(int key, int value) 
+    {
+        a[key] = value;
     }
-    int get(int key) {
+    
+    int get(int key) 
+    {
         return a[key];
     }
-    void remove(int key) {
-        a[key]=-1;
+    
+    void remove(int key) 
+    {
+        a[key] = -1;
     }
 };
+
 /**
  * Your MyHashMap object will be instantiated and called as such:
  * MyHashMap* obj = new MyHashMap();
