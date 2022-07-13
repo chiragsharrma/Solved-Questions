d94b263ec3d661b88874ecc6dbda4ca3d7144f2a
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-07-14 01:15:04 +0530

    Time: 11 ms (59.71%), Space: 6.7 MB (62.25%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -11,11 +11,14 @@ public:
         for (int i = num1.size() - 1; i >= 0; --i) {
             for (int j = num2.size() - 1; j >= 0; --j) {
                 num[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
-                num[i + j] += num[i + j + 1] / 10;
-                num[i + j + 1] %= 10;
+                num[i + j] += num[i + j + 1] / 10;//it is giving remainder
+                num[i + j + 1] %= 10;   // it is giving quoteint 
             }
-        }
-        
+        }               
+       //3 4 8 8
+       //  2 3 4
+       // 1 3 9 5 2
+                
         // skip leading 0's
         int i = 0;
         while (i < num.size() && num[i] == 0) ++i;
