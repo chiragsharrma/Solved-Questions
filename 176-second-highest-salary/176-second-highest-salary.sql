1a1bd6c9830fa5b0779af92cb1262c80f92f0bef
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-08-19 23:03:30 +0530

    Time: 324 ms (44.89%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,2 +1,3 @@
-select max(salary) as 'SecondHighestSalary' from Employee
-where salary<>(select max(salary) from Employee);
+# Write your MySQL query statement below
+select max(salary) as SecondHighestSalary from employee
+where salary != (select max(salary) from employee) and salary is not null ;
