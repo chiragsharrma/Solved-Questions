43e7fccb8194d27d798337a707e4abd0c9d02d08
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-08-23 04:23:04 +0530

    Time: 2 ms (65.34%), Space: 41.6 MB (25.85%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -4,9 +4,9 @@ class Solution {
         if(n==0) return false;
         if(n==1) return true;
         
-        while(n%4==0)
+        while(n%4==0)//checking if it gets fully devided by 4 and while it gets than 
         {
-            n/=4;
+            n/=4; //then subtracting every part of 4 from it.
         }
         return n==1;  
     }
