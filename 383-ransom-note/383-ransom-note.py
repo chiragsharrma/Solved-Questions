280c7d8615b79c4c1a6b44175135453eb723446d
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-08-26 02:05:35 +0530

    Time: 73 ms (73.69%), Space: 14.2 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,10 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for c in ransomNote:
-            if c not in magazine:
-                return False
-            magazine = magazine.replace(c,'',1)  #as each letter in magazine can only be used once in ransom so as soon as we find it we take its count and replace it with ''.
-        return True    
-         
-        
-        
+        return not collections.Counter(ransomNote) - collections.Counter(magazine)
