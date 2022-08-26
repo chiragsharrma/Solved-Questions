9b0b83738a7d29ace152ac52bdf5135ef0656361
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-08-27 04:35:30 +0530

    Time: 78 ms (32.09%), Space: 13.8 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,6 +1,6 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         for i in range(32):
-            if Counter(str(n))==Counter(str(2**i)):
+            if Counter(str(n))==Counter(str(2**i)):#str method returns string version of int
                 return True
         return False
