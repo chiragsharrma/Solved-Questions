0bad79c99662d8d45848cb9bf3a2c4159ef26ff0
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-08-27 04:23:21 +0530

    Time: 40 ms (86.63%), Space: 13.9 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,22 +1,6 @@
-class Solution {
-public:
-    bool reorderedPowerOf2(int n) {
-        string s = to_string(n);
-        sort(s.begin(),s.end());
-		
-        vector<string> power;
-        for(int i=0;i<=30;i++){
-            int p = pow(2,i);
-            power.push_back(to_string(p));
-        }
-        
-        for(int i=0;i<=30;i++){
-            sort(power[i].begin(),power[i].end());
-        }
-        
-        for(int i=0;i<=30;i++){
-            if(power[i] == s ) return true;
-        }
-        return false;
-    }
-};
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        for i in range(32):
+            if Counter(str(n))==Counter(str(2**i)):
+                return True
+        return False
