d633581e1b7df9643f2e816680fdb26aa841d41c
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-09-08 00:03:29 +0530

    Time: 71 ms (11.80%), Space: 53.3 MB (42.58%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+    string tree2str(TreeNode* root) 
+    {
+        if(root == NULL) return "";
+        
+        string ans = to_string(root->val);
+        
+        if(root->left)
+        {
+            ans += "(";
+            ans += tree2str(root->left);
+            ans += ")";
+        }
+        
+        if(root->right)
+        {
+            if(!root->left) ans +="()";
+            ans += "(";
+            ans += tree2str(root->right);
+            ans += ")";
+        }
+        return ans;
+    }
+};
