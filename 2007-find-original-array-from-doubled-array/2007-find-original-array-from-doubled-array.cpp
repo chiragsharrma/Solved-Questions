da5ed1c2641ff4348b437aecc11cf0159da670ab
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-09-16 02:08:46 +0530

    Time: 867 ms (25.96%), Space: 141.1 MB (68.37%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        int n = changed.size();
+        vector<int> ans;
+        vector<int> vacans;
+		
+         unordered_map<int, int> um;
+        if (n % 2 !=0) return ans;
+        sort(changed.begin(), changed.end());
+	
+        for(auto x: changed){
+            um[x]++;
+			
+        }
+        for (auto y: changed) {
+          if (um[y] == 0) continue;
+          if (um[y * 2] == 0) return vacans;
+          ans.push_back(y);
+          um[y]--;
+          um[y * 2]--;
+        }
+        return ans;
+    }
+};
