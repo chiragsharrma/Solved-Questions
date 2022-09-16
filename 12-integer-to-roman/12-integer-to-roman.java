83dd9ffad6d66355e1422fba0c1f9a0b65b26d23
CHIRAG SHARMA <84849891+chiragsharrma@users.noreply.github.com>
2022-09-17 01:12:08 +0530

    Time: 14 ms (34.25%), Space: 46.9 MB (21.67%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String intToRoman(int num) 
+    {
+        int[] integer = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+        String ans = "";
+        
+        for(int i=0;i<13;i++)
+        {
+            int value = integer[i];
+            
+            while(num >= value)
+            {
+                ans += roman[i];
+                num -= value;
+            }
+        }
+        
+        return ans;
+    }
+}
